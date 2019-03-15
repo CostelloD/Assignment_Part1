48c795d8a4ae33719c6b403ba11f7a8728e20034
CostelloD <damien.costello2@mail.itsligo.ie>
2019-03-15 01:30:58 +0000

    List page stuff

--- EntAppSecond/Models/Student.cs
+++ EntAppSecond/Models/Student.cs
@@ -13,12 +13,12 @@ namespace EntAppSecond.Models
     {
         [Key]
         [Display(Name = "First Name")]
-        [Required(ErrorMessage = "Please enter a First Name")]
+        [Required(ErrorMessage = "Please enter a First for the child")]
         [MinLength(2, ErrorMessage = "First Name must contain at least two characters")]
         public string ChildFirstNAme { get; set; }
 
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a surname for the child")]
         [DisplayName("Surname")]
         [MinLength(3, ErrorMessage = "Surname Name must contain at least three characters")]
         public string ChildSurname { get; set; }
@@ -27,7 +27,7 @@ namespace EntAppSecond.Models
         [RegularExpression(@"(s|S)\d{7}", ErrorMessage = "Not an valid PPS Number")]
         public string PPSNumber { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a date of birth for the child")]
         [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
@@ -37,23 +37,17 @@ namespace EntAppSecond.Models
 
 
 
-
-
-
-
-
-
         [Display(Name = "First Name")]
         [Required(ErrorMessage = "Please enter a First Name")]
         [MinLength(2, ErrorMessage = "First Name must contain at least two characters")]
         public string FirstName { get; set; } = "";
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a Second Name")]
         [DisplayName("Second Name")]
         [MinLength(3, ErrorMessage = "First Name must contain at least three characters")]
         public string SecondName { get; set; } = "";
 
-        /*use area for this*/
+
         [Required(ErrorMessage = "Please enter your address")]
         public string Address { get; set; }
 
@@ -62,7 +56,7 @@ namespace EntAppSecond.Models
 
 
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your Mobile Phone Number")]
         [Display(Name = "Mobile Phone Number")]
         [RegularExpression(@"^(\D?)(\d{2,5})?\D?\d{2,3}(\D?)(\D?)\d{7}", ErrorMessage = "Not a Valid Mobile Number, please check and try again")]
         [DataType(DataType.PhoneNumber)]
@@ -72,13 +66,14 @@ namespace EntAppSecond.Models
         [DataType(DataType.PhoneNumber)]
         public string PhoneSecond { get; set; }
 
+        [Required(ErrorMessage = "Please enter an alternate contact number")]
         [DataType(DataType.PhoneNumber)]
         public string PhoneThird { get; set; }
 
+        [Required(ErrorMessage = "Please enter an email address")]
         [DataType(DataType.PhoneNumber)]
         public string FirstEmailAddress { get; set; }
 
-        [Required(ErrorMessage = "Please enter an email address")]
         [DataType(DataType.EmailAddress)]
         public string SecondEmailAddress { get; set; }
 
@@ -86,7 +81,7 @@ namespace EntAppSecond.Models
         [Required(ErrorMessage = "Please select the days you with your child to attend")]
         public int DaysRequested { get; set; }
 
-        [Required(ErrorMessage = "Please Select Partime of Full time option")]
+        [Required(ErrorMessage = "Please Select Partime of Fulltime option")]
         public int HoursRequested { get; set; }
 
         [Required(ErrorMessage = "Please Select a Startdate for your child")]
